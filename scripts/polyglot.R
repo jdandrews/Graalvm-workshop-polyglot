191c69a78ad98b7ffbdbe6c41884ae017c9c9a16
Jerry Andrews <jerry@jrandrews.org>
2022-12-09 01:00:13 +0000

    scale the graph

--- scripts/polyglot.R
+++ scripts/polyglot.R
@@ -2,7 +2,7 @@ library("asciichartr")
 
 toPlot <- function(data) {
     y = scan(textConnection(data))
-    cat(asciiPlot(y, cfg = list('height' = 20)))
+    cat(asciiPlot(y, cfg = list('height' = 14)))
 }
 
 export('toPlot', toPlot)
